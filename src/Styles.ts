819905c0c4f3b9ac030422e64d0278174f44ff78
Raphael Bouças <rafaladob@gmail.com>
2024-04-08 16:35:54 -0300

    configurando o ambiente de desenvolvimento

---
+++ src/Styles.ts
@@ -0,0 +1,11 @@
+import {createGlobalStyle} from 'styled-components'
+
+const EstiloGlobal = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    background-color: grey
+  }
+`
+
+export default EstiloGlobal
