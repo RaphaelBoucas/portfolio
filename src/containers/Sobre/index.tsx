5e4646770f1f7a006613cb7f49588613868a8c33
Raphael Bouças <rafaladob@gmail.com>
2024-04-14 18:27:38 -0300

    criando a secao de projetos e o componente de projetos

--- src/containers/Sobre/index.tsx
+++ src/containers/Sobre/index.tsx
@@ -7,7 +7,7 @@ import { Githubsecao } from "./styles";
 const Sobre = () => (
   <section>
     <Titulo fontSize={16}>Sobre</Titulo>
-    <Paragrafo tipo='secundario'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi veniam consectetur non sed enim voluptatem cupiditate repellat tempore doloremque eius! Omnis laudantium id rem voluptate deserunt tenetur hic quam numquam.</Paragrafo>
+    <Paragrafo tipo="principal">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi veniam consectetur non sed enim voluptatem cupiditate repellat tempore doloremque eius! Omnis laudantium id rem voluptate deserunt tenetur hic quam numquam.</Paragrafo>
     <Githubsecao>
     <img src="https://github-readme-stats.vercel.app/api?username=raphaelboucas&show_icons=true&theme=dracula&include_all_commits=true&count_private=true"/>
     <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=raphaelboucas&layout=compact&langs_count=7&theme=dracula"/>
